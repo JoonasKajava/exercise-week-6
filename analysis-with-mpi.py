083ad708e2a45725decafb7d6563befd644b4e2c
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-04-18 16:30:06 +0300

    progress

--- analysis-with-mpi.py
+++ analysis-with-mpi.py
@@ -11,6 +11,8 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
+if rank == 0:
+    print("size:", size)
 print("rank:", rank)
 
 start = time.time()
--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 #SBATCH --account=project_2018026
 #SBATCH --job-name=analysis
 #SBATCH --time=00:05:00
+#SBATCH --ntasks=32
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
 #SBATCH --partition=small
 module load python-data
+module load openmpi
 
 srun python analysis-with-mpi.py /projappl/project_2018026/super_data
