c3068571af3f049e348e22ef91bacdea565f0ec2
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-04-18 16:20:15 +0300

    progress

--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@
 #SBATCH --account=project_2018026
 #SBATCH --job-name=analysis
 #SBATCH --time=00:05:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
 #SBATCH --partition=small
 module load python-data
-srun python analysis.py /projappl/project_2018026/super_data
+
+srun python analysis-with-mpi.py /projappl/project_2018026/super_data
